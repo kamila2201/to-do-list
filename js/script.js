cab82a8a473b91bb69f74899f15d3ac47d66795c
Kamila Maciąg <kamila.maciag22@gmail.com>
2023-02-21 18:30:03 +0100

    changed functions to comply with immutability

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
 {
-  const tasks = [];
+  let tasks = [];
 
   const addNewTask = (newTaskContent) => {
-    tasks.push({
-      content: newTaskContent,
-    });
-
+    tasks = [
+      ...tasks,
+      { content: newTaskContent },
+    ];
     render();
   };
 
   const removeTask = (taskIndex) => {
-    tasks.splice(taskIndex, 1);
-
+    tasks = [
+      ...tasks.slice(0, taskIndex),
+      ...tasks.slice(taskIndex + 1),
+    ];
     render();
   };
 
   const toggleTaskDone = (taskIndex) => {
-    tasks[taskIndex].done = !tasks[taskIndex].done;
+    tasks = tasks.map((task, index) => index === taskIndex ? { ...task, done: !task.done } : task);
     render();
   };
 
@@ -58,7 +60,6 @@
     renderTasks();
 
     bindButtonsEvents();
-
   };
 
   const onFormSubmit = (event) => {
